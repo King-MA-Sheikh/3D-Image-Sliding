42d434216fcaae8ea155c1f99b3026f90515e20b
king-MA_Sheikh <161627033+King-MA-Sheikh@users.noreply.github.com>
2025-07-21 14:35:10 +0530

    Create jenkinsfile.groovy

---
+++ jenkinsfile.groovy
@@ -0,0 +1,43 @@
+@Library("Shared") _
+
+pipeline {
+    agent { label "vinod" }
+
+    stages {
+        stage("Code") {
+            steps {
+                script {
+                    clone('https://github.com/King-MA-Sheikh/3D-Image-Sliding.git', 'main')
+                }
+            }
+        }
+
+        stage("Build") {
+            steps {
+                script {
+                    sh '''
+                        export PATH=$PATH:/usr/bin
+                        docker --version
+                        docker build -t 3dimagesliding:latest .
+                    '''
+                }
+            }
+        }
+
+        stage("Push to DockerHub") {
+            steps {
+                script {
+                    docker_push("3dimagesliding", "latest", "maspro900")
+                }
+            }
+        }
+
+        stage("Deploy") {
+            steps {
+                echo "Deploying container"
+                sh 'docker compose down || true'
+                sh 'docker compose up -d'
+            }
+        }
+    }
+}
